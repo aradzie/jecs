5245eeb41b281f952a15537420f6b7548f3850b8
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-09-22 16:04:26 +0200

    Rewrite tests using netlist

--- packages/simulator/src/simulation/device/vcisource.test.ts
+++ packages/simulator/src/simulation/device/vcisource.test.ts
@@ -1,36 +1,20 @@
 import test from "ava";
-import { Circuit } from "../circuit";
-import { Resistor } from "./resistor";
-import { VCISource } from "./vcisource";
-import { VSource } from "./vsource";
+import { readNetlist } from "../netlist";
 
 test("voltage controlled current source", (t) => {
-  const circuit = new Circuit();
-  const ng = circuit.groundNode;
-  const nib = circuit.allocNode("NIB");
-  const nob = circuit.allocNode("NOB");
-  circuit.addDevice(
-    new VSource([ng, nib], {
-      name: "V1",
-      v: 3.0,
-    }),
-    new VCISource([ng, nib, ng, nob], {
-      name: "VCI1",
-      gain: 0.5,
-    }),
-    new Resistor([ng, nob], {
-      name: "R1",
-      r: 100.0,
-    }),
-  );
+  const circuit = readNetlist([
+    ["v", ["g", "NIB"], { name: "V1", v: 3 }],
+    ["vcis", ["g", "NIB", "g", "NOB"], { name: "VCI1", gain: 0.5 }],
+    ["r", ["g", "NOB"], { name: "R1", r: 100 }],
+  ]);
   const r = circuit.dc();
   t.deepEqual(
     r,
     new Map([
       ["V[GROUND]", 0],
       ["V[NIB]", 3],
-      ["V[NOB]", 150],
       ["I[GROUND->NIB]", 0],
+      ["V[NOB]", 150],
     ]),
   );
 });
--- packages/simulator/src/simulation/device/vcvsource.test.ts
+++ packages/simulator/src/simulation/device/vcvsource.test.ts
@@ -1,36 +1,20 @@
 import test from "ava";
-import { Circuit } from "../circuit";
-import { Resistor } from "./resistor";
-import { VCVSource } from "./vcvsource";
-import { VSource } from "./vsource";
+import { readNetlist } from "../netlist";
 
 test("voltage controlled voltage source", (t) => {
-  const circuit = new Circuit();
-  const ng = circuit.groundNode;
-  const nib = circuit.allocNode("NIB");
-  const nob = circuit.allocNode("NOB");
-  circuit.addDevice(
-    new VSource([ng, nib], {
-      name: "V1",
-      v: 3.0,
-    }),
-    new VCVSource([ng, nib, ng, nob], {
-      name: "VCV1",
-      gain: 0.5,
-    }),
-    new Resistor([ng, nob], {
-      name: "R1",
-      r: 1000.0,
-    }),
-  );
+  const circuit = readNetlist([
+    ["v", ["g", "NIB"], { name: "V1", v: 3 }],
+    ["vcvs", ["g", "NIB", "g", "NOB"], { name: "VCV1", gain: 0.5 }],
+    ["r", ["g", "NOB"], { name: "R1", r: 1000 }],
+  ]);
   const r = circuit.dc();
   t.deepEqual(
     r,
     new Map([
       ["V[GROUND]", 0],
       ["V[NIB]", 3],
-      ["V[NOB]", 1.5],
       ["I[GROUND->NIB]", 0],
+      ["V[NOB]", 1.5],
       ["I[GROUND->NOB]", -0.0015],
     ]),
   );
