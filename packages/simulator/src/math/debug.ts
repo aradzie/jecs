527b2c5519b4cd379ccbbca1be8c817f36f510dc
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-09-22 14:42:01 +0200

    Improve debug utilities

--- packages/simulator/src/math/debug.ts
+++ packages/simulator/src/math/debug.ts
@@ -12,17 +12,41 @@ export function toArray(matrix: Matrix): number[][] {
   return Array.from(matrix).map((row) => [...row]);
 }
 
-export function dumpMatrix(m: MatrixLike): void {
+export function dumpMatrix(m: MatrixLike, separator = "|"): void {
   for (let i = 0; i < m.length; i++) {
     const row = m[i];
     const s = [];
     for (let j = 0; j < row.length; j++) {
-      s.push(String(round(row[j])).padStart(5));
+      s.push(printValue(row[j]));
     }
-    console.log(s.join("|"));
+    console.log(s.join(separator));
   }
 }
 
-export function round(n: number): number {
-  return Math.round(n * 100) / 100;
+export function dumpVector(v: VectorLike, separator = "|"): void {
+  const s = [];
+  for (let i = 0; i < v.length; i++) {
+    s.push(printValue(v[i]));
+  }
+  console.log(s.join(separator));
+}
+
+export function dumpSle(m: MatrixLike, v: VectorLike, separator = "|"): void {
+  for (let i = 0; i < m.length; i++) {
+    const row = m[i];
+    const s = [];
+    for (let j = 0; j < row.length; j++) {
+      s.push(String(round(row[j])).padStart(7));
+    }
+    s.push(printValue(v[i]));
+    console.log(s.join(separator));
+  }
+}
+
+function printValue(x: number) {
+  return String(round(x)).padStart(7);
+}
+
+function round(n: number): number {
+  return Math.round(n * 10000) / 10000;
 }
