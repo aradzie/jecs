3330300eab7b972a27c833f40326f6f651690c2c
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-09-22 18:01:49 +0200

    Extract some functions in a separate file

---
+++ packages/simulator/src/simulation/device/devicemap.ts
@@ -0,0 +1,72 @@
+import { CircuitError } from "../error";
+import type { Node } from "../network";
+import {
+  Device,
+  DeviceClass,
+  RawDeviceProps,
+  validateDeviceProps,
+} from "./device";
+import { devices } from "./index";
+
+const deviceMap = new Map<string, DeviceClass>();
+
+export function registerDevice(...deviceClasses: DeviceClass[]) {
+  for (const deviceClass of deviceClasses) {
+    const { id, numTerminals, propsSchema } = deviceClass;
+    if (id == null) {
+      throw new CircuitError(
+        `The [id] attribute is missing` + //
+          ` in device class [${deviceClass}]`,
+      );
+    }
+    if (numTerminals == null) {
+      throw new CircuitError(
+        `The [numTerminals] attribute is missing in` + //
+          ` device class [${deviceClass}]`,
+      );
+    }
+    if (propsSchema == null) {
+      throw new CircuitError(
+        `The [propsSchema] attribute is missing` + //
+          ` in device class [${deviceClass}]`,
+      );
+    }
+    if (deviceMap.has(id)) {
+      throw new CircuitError(`Duplicate device id [${id}]`);
+    }
+    deviceMap.set(id, deviceClass);
+  }
+}
+
+export function createDevice(
+  id: string,
+  nodes: readonly Node[],
+  rawProps: RawDeviceProps,
+): Device {
+  const deviceClass = deviceMap.get(id) ?? null;
+  if (deviceClass == null) {
+    throw new CircuitError(`Unknown device id [${id}]`);
+  }
+  const { name } = rawProps;
+  if (typeof name !== "string") {
+    throw new CircuitError(`The [name] property is missing`);
+  }
+  const { numTerminals, propsSchema } = deviceClass;
+  if (nodes.length !== numTerminals) {
+    throw new CircuitError(
+      `Netlist error in device [${id}:${name}]: ` +
+        `Invalid number of terminals`,
+    );
+  }
+  let props;
+  try {
+    props = validateDeviceProps(rawProps, propsSchema);
+  } catch (err) {
+    throw new CircuitError(
+      `Netlist error in device [${id}:${name}]: ` + err.message,
+    );
+  }
+  return new deviceClass(nodes, props);
+}
+
+registerDevice(...devices);
--- packages/simulator/src/simulation/netlist.ts
+++ packages/simulator/src/simulation/netlist.ts
@@ -1,10 +1,8 @@
 import { Circuit } from "./circuit";
-import { devices } from "./device";
-import type { Device, DeviceClass, RawDeviceProps } from "./device/device";
-import { validateDeviceProps } from "./device/device";
+import type { RawDeviceProps } from "./device/device";
+import { createDevice } from "./device/devicemap";
 import { Ground } from "./device/ground";
 import { VSource } from "./device/vsource";
-import { CircuitError } from "./error";
 import type { Node } from "./network";
 
 export type Netlist = readonly NetlistItem[];
@@ -14,67 +12,6 @@ export type NetlistItem = readonly [
   connections: readonly string[],
   rawProps: RawDeviceProps,
 ];
-
-const deviceMap = new Map<string, DeviceClass>();
-
-export function registerDevice(...deviceClasses: DeviceClass[]) {
-  for (const deviceClass of deviceClasses) {
-    const { id, numTerminals, propsSchema } = deviceClass;
-    if (id == null) {
-      throw new CircuitError(
-        `The [id] attribute is missing` + //
-          ` in device class [${deviceClass}]`,
-      );
-    }
-    if (numTerminals == null) {
-      throw new CircuitError(
-        `The [numTerminals] attribute is missing in` + //
-          ` device class [${deviceClass}]`,
-      );
-    }
-    if (propsSchema == null) {
-      throw new CircuitError(
-        `The [propsSchema] attribute is missing` + //
-          ` in device class [${deviceClass}]`,
-      );
-    }
-    if (deviceMap.has(id)) {
-      throw new CircuitError(`Duplicate device id [${id}]`);
-    }
-    deviceMap.set(id, deviceClass);
-  }
-}
-
-export function createDevice(
-  id: string,
-  nodes: readonly Node[],
-  rawProps: RawDeviceProps,
-): Device {
-  const deviceClass = deviceMap.get(id) ?? null;
-  if (deviceClass == null) {
-    throw new CircuitError(`Unknown device id [${id}]`);
-  }
-  const { name } = rawProps;
-  if (typeof name !== "string") {
-    throw new CircuitError(`The [name] property is missing`);
-  }
-  const { numTerminals, propsSchema } = deviceClass;
-  if (nodes.length !== numTerminals) {
-    throw new CircuitError(
-      `Netlist error in device [${id}:${name}]: ` +
-        `Invalid number of terminals`,
-    );
-  }
-  let props;
-  try {
-    props = validateDeviceProps(rawProps, propsSchema);
-  } catch (err) {
-    throw new CircuitError(
-      `Netlist error in device [${id}:${name}]: ` + err.message,
-    );
-  }
-  return new deviceClass(nodes, props);
-}
 
 export function readNetlist(netlist: Netlist): Circuit {
   const circuit = new Circuit();
@@ -115,5 +52,3 @@ export function readNetlist(netlist: Netlist): Circuit {
 
   return circuit;
 }
-
-registerDevice(...devices);
