e734f786689bec068d1f8140ca5456342d8305c0
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-09-23 09:39:34 +0200

    Use private fields and getters

--- packages/simulator/src/simulation/circuit.ts
+++ packages/simulator/src/simulation/circuit.ts
@@ -4,47 +4,59 @@ import { CircuitError } from "./error";
 import { Branch, Network, Node } from "./network";
 
 export class Circuit implements Network {
-  readonly groundNode: Node;
-  readonly nodes: (Node | Branch)[] = [];
-  readonly nodeNames = new Set<string>();
-  readonly devices: Device[] = [];
-  readonly deviceNames = new Set<string>();
+  readonly #groundNode: Node;
+  readonly #nodes: (Node | Branch)[] = [];
+  readonly #nodeNames = new Set<string>();
+  readonly #devices: Device[] = [];
+  readonly #deviceNames = new Set<string>();
 
   constructor() {
-    this.groundNode = new Node(-1, "GROUND");
+    this.#groundNode = new Node(-1, "GROUND");
+  }
+
+  get groundNode(): Node {
+    return this.#groundNode;
+  }
+
+  get nodes(): readonly (Node | Branch)[] {
+    return this.#nodes;
+  }
+
+  get devices(): readonly Device[] {
+    return this.#devices;
   }
 
   allocNode(name: string): Node {
-    if (this.nodeNames.has(name)) {
+    if (this.#nodeNames.has(name)) {
       throw new CircuitError(`Duplicate node name [${name}]`);
     }
-    this.nodeNames.add(name);
-    const node = new Node(this.nodes.length, name);
-    this.nodes.push(node);
+    this.#nodeNames.add(name);
+    const node = new Node(this.#nodes.length, name);
+    this.#nodes.push(node);
     return node;
   }
 
   allocBranch(a: Node, b: Node): Branch {
-    const branch = new Branch(this.nodes.length, a, b);
-    this.nodes.push(branch);
+    const branch = new Branch(this.#nodes.length, a, b);
+    this.#nodes.push(branch);
     return branch;
   }
 
   addDevice(...devices: readonly Device[]): void {
     for (const device of devices) {
       const { name } = device;
-      if (this.deviceNames.has(name)) {
+      if (this.#deviceNames.has(name)) {
         throw new CircuitError(`Duplicate device name [${name}]`);
       }
-      this.deviceNames.add(device.name);
-      this.devices.push(device);
+      this.#deviceNames.add(device.name);
+      this.#devices.push(device);
       device.connect(this);
     }
   }
 
   updateDevices(x: Vector): void {
-    for (let i = 0; i < this.nodes.length; i++) {
-      const node = this.nodes[i];
+    for (let i = 0; i < this.#nodes.length; i++) {
+      const node = this.#nodes[i];
       if (node instanceof Node) {
         node.voltage = x[i];
         continue;
@@ -55,7 +67,7 @@ export class Circuit implements Network {
       }
     }
 
-    for (const device of this.devices) {
+    for (const device of this.#devices) {
       device.update();
     }
   }
