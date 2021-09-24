71b58f84092d8e1b3af0b52cba7d5c1c76f1edc7
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-09-24 12:24:13 +0200

    Format numbers with SI suffixes and units

---
+++ packages/simulator/src/util/format.ts
@@ -0,0 +1,35 @@
+import { getUnitSymbol, Unit } from "./unit";
+
+export function formatNumber(v: number, unit = Unit.UNITLESS): string {
+  const unitName = getUnitSymbol(unit);
+  const a = Math.abs(v);
+  if (a < 1e-15) {
+    return `0${unitName}`;
+  }
+  if (a < 1e-9) {
+    return `${format(v * 1e12)}p${unitName}`;
+  }
+  if (a < 1e-6) {
+    return `${format(v * 1e9)}n${unitName}`;
+  }
+  if (a < 1e-3) {
+    return `${format(v * 1e6)}μ${unitName}`;
+  }
+  if (a < 1) {
+    return `${format(v * 1e3)}m${unitName}`;
+  }
+  if (a < 1e3) {
+    return `${format(v)}${unitName}`;
+  }
+  if (a < 1e6) {
+    return `${format(v * 1e-3)}k${unitName}`;
+  }
+  if (a < 1e9) {
+    return `${format(v * 1e-6)}M${unitName}`;
+  }
+  return `${format(v * 1e-9)}G${unitName}`;
+}
+
+function format(n: number): string {
+  return String(Math.round(n * 1000) / 1000);
+}
