968362905df8b3263358b03609441a29ab2980d7
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-10-14 11:38:34 +0200

    Remove the ui package

--- packages/ui/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from "react-dom";
-import { App } from "./ui/App";
-import "./ui/styles.css";
-
-render(<App />, document.getElementById("root"));
--- packages/ui/src/ui/StatusBar.tsx
+++
@@ -1,9 +0,0 @@
-import type { FunctionComponent, ReactNode } from "react";
-
-export interface StatusBarProps {
-  readonly children: ReactNode;
-}
-
-export const StatusBar: FunctionComponent<StatusBarProps> = ({ children }) => {
-  return <div className="StatusBar">{children}</div>;
-};
