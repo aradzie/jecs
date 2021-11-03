49b8575985283eeb39076d88719abbed34581a92
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2021-11-03 11:00:33 +0100

    Refactor JFET state management

--- packages/simulator/src/device/nonlinear/jfet.ts
+++ packages/simulator/src/device/nonlinear/jfet.ts
@@ -32,8 +32,6 @@ const enum S {
   Ggd,
   /** Jfet drain-source voltage. */
   Vds,
-  /** Jfet source-drain voltage. */
-  Vsd,
   /** Jfet drain-source current. */
   Ids,
   /** Jfet drain-source conductance. */
@@ -141,48 +139,54 @@ export class Jfet extends Device<JfetParams> {
     this.pnGd = new PN(Is, N, Temp);
   }
 
-  override eval(state: DeviceState): void {
+  override eval(state: DeviceState, final: boolean): void {
     const { params, ns, ng, nd, pnGs, pnGd } = this;
     const { polarity, Vth, beta, lambda } = params;
-    const sign = fetSign(polarity);
+    const pol = fetSign(polarity);
 
-    const Vgs = (state[S.Vgs] = pnGs.limitVoltage(sign * (ng.voltage - ns.voltage), state[S.Vgs]));
-    const Vgd = (state[S.Vgd] = pnGd.limitVoltage(sign * (ng.voltage - nd.voltage), state[S.Vgd]));
+    // VOLTAGES
+
+    let Vgs = pol * (ng.voltage - ns.voltage);
+    let Vgd = pol * (ng.voltage - nd.voltage);
+    if (!final) {
+      Vgs = pnGs.limitVoltage(Vgs, pol * state[S.Vgs]);
+      Vgd = pnGd.limitVoltage(Vgd, pol * state[S.Vgd]);
+    }
+    const Vds = Vgs - Vgd;
 
     // DIODES
 
-    state[S.Igs] = pnGs.evalCurrent(Vgs);
-    state[S.Ggs] = pnGs.evalConductance(Vgs);
-
-    state[S.Igd] = pnGs.evalCurrent(Vgd);
-    state[S.Ggd] = pnGs.evalConductance(Vgd);
+    const Igs = pnGs.evalCurrent(Vgs);
+    const Ggs = pnGs.evalConductance(Vgs);
+    const Igd = pnGs.evalCurrent(Vgd);
+    const Ggd = pnGs.evalConductance(Vgd);
 
     // FET
 
-    const Vds = (state[S.Vds] = Vgs - Vgd);
-    const Vsd = (state[S.Vsd] = Vgd - Vgs);
-
+    let Ids;
+    let Gds;
+    let Gm;
     if (Vgs >= Vgd) {
       // Normal mode.
       const Vgst = Vgs - Vth;
       if (Vgst <= 0) {
         // Cutoff region.
-        state[S.Ids] = 0;
-        state[S.Gds] = 0;
-        state[S.Gm] = 0;
+        Ids = 0;
+        Gds = 0;
+        Gm = 0;
       } else {
         const c0 = lambda * Vds;
         const c1 = beta * (1 + c0);
         if (Vgst <= Vds) {
           // Saturation region.
-          state[S.Ids] = c1 * Vgst * Vgst;
-          state[S.Gds] = beta * lambda * Vgst * Vgst;
-          state[S.Gm] = 2 * c1 * Vgst;
+          Ids = c1 * Vgst * Vgst;
+          Gds = beta * lambda * Vgst * Vgst;
+          Gm = 2 * c1 * Vgst;
         } else {
           // Linear region.
-          state[S.Ids] = c1 * Vds * (2 * Vgst - Vds);
-          state[S.Gds] = 2 * c1 * (Vgst - Vds) + beta * c0 * (2 * Vgst - Vds);
-          state[S.Gm] = 2 * c1 * Vds;
+          Ids = c1 * Vds * (2 * Vgst - Vds);
+          Gds = 2 * c1 * (Vgst - Vds) + beta * c0 * (2 * Vgst - Vds);
+          Gm = 2 * c1 * Vds;
         }
       }
     } else {
@@ -190,42 +194,62 @@ export class Jfet extends Device<JfetParams> {
       const Vgdt = Vgd - Vth;
       if (Vgdt <= 0) {
         // Cutoff region.
-        state[S.Ids] = 0;
-        state[S.Gds] = 0;
-        state[S.Gm] = 0;
+        Ids = 0;
+        Gds = 0;
+        Gm = 0;
       } else {
+        const Vsd = -Vds;
         const c0 = lambda * Vsd;
         const c1 = beta * (1 + c0);
         if (Vgdt <= Vsd) {
           // Saturation region.
-          state[S.Ids] = -c1 * Vgdt * Vgdt;
-          state[S.Gds] = beta * lambda * Vgdt * Vgdt + 2 * c1 * Vgdt;
-          state[S.Gm] = -2 * c1 * Vgdt;
+          Ids = -c1 * Vgdt * Vgdt;
+          Gds = beta * lambda * Vgdt * Vgdt + 2 * c1 * Vgdt;
+          Gm = -2 * c1 * Vgdt;
         } else {
           // Linear region.
-          state[S.Ids] = -c1 * Vsd * (2 * Vgdt - Vsd);
-          state[S.Gds] = 2 * c1 * Vgdt + beta * c0 * (2 * Vgdt - Vsd);
-          state[S.Gm] = -2 * c1 * Vsd;
+          Ids = -c1 * Vsd * (2 * Vgdt - Vsd);
+          Gds = 2 * c1 * Vgdt + beta * c0 * (2 * Vgdt - Vsd);
+          Gm = -2 * c1 * Vsd;
         }
       }
     }
+
+    // VOLTAGES
+
+    state[S.Vgs] = pol * Vgs;
+    state[S.Vgd] = pol * Vgd;
+    state[S.Vds] = pol * Vds;
+
+    // DIODES
+
+    state[S.Igs] = pol * Igs;
+    state[S.Ggs] = Ggs;
+    state[S.Igd] = pol * Igd;
+    state[S.Ggd] = Ggd;
+
+    // FET
+
+    state[S.Ids] = pol * Ids;
+    state[S.Gds] = Gds;
+    state[S.Gm] = Gm;
   }
 
   override stamp(
     stamper: Stamper,
-    [Vgs, Igs, Ggs, Vgd, Igd, Ggd, Vds, Vsd, Ids, Gds, Gm]: DeviceState,
+    [Vgs, Igs, Ggs, Vgd, Igd, Ggd, Vds, Ids, Gds, Gm]: DeviceState,
   ): void {
     const { ns, ng, nd, params } = this;
     const { polarity } = params;
-    const sign = fetSign(polarity);
+    const pol = fetSign(polarity);
 
     // DIODES
 
     stamper.stampConductance(ng, ns, Ggs);
-    stamper.stampCurrentSource(ng, ns, sign * (Igs - Ggs * Vgs));
+    stamper.stampCurrentSource(ng, ns, pol * (pol * Igs - pol * Ggs * Vgs));
 
     stamper.stampConductance(ng, nd, Ggd);
-    stamper.stampCurrentSource(ng, nd, sign * (Igd - Ggd * Vgd));
+    stamper.stampCurrentSource(ng, nd, pol * (pol * Igd - pol * Ggd * Vgd));
 
     // FET
 
@@ -234,23 +258,17 @@ export class Jfet extends Device<JfetParams> {
     stamper.stampMatrix(nd, ns, -Gm);
     stamper.stampMatrix(ns, ng, -Gm);
     stamper.stampMatrix(ns, ns, Gm);
-    stamper.stampCurrentSource(nd, ns, sign * (Ids - Gds * Vds - Gm * Vgs));
+    stamper.stampCurrentSource(nd, ns, pol * (pol * Ids - pol * Gds * Vds - pol * Gm * Vgs));
   }
 
   override ops(
-    [VgsX, Igs, Ggs, VgdX, Igd, Ggd, VdsX, VsdX, Ids, Gds, Gm]: DeviceState = this.state,
+    [Vgs, Igs, Ggs, Vgd, Igd, Ggd, Vds, Ids, Gds, Gm]: DeviceState = this.state,
   ): readonly Op[] {
-    const { ns, ng, nd, params } = this;
-    const { polarity } = params;
-    const sign = fetSign(polarity);
-    const Vgs = sign * (ng.voltage - ns.voltage);
-    const Vgd = sign * (ng.voltage - nd.voltage);
-    const Vds = sign * (nd.voltage - ns.voltage);
     return [
-      { name: "Vgs", value: sign * Vgs, unit: Unit.VOLT },
-      { name: "Vgd", value: sign * Vgd, unit: Unit.VOLT },
-      { name: "Vds", value: sign * Vds, unit: Unit.VOLT },
-      { name: "Ids", value: sign * Ids, unit: Unit.AMPERE },
+      { name: "Vgs", value: Vgs, unit: Unit.VOLT },
+      { name: "Vgd", value: Vgd, unit: Unit.VOLT },
+      { name: "Vds", value: Vds, unit: Unit.VOLT },
+      { name: "Ids", value: Ids, unit: Unit.AMPERE },
     ];
   }
 }
