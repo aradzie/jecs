eea7ac21cca5351779b503764bb006475177ea39
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2022-04-18 11:13:17 +0200

    Update dependencies

--- packages/shell/src/main.tsx
+++ packages/shell/src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")!,
-);
+const container = document.getElementById("root");
+if (container != null) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  throw new Error();
+}
