4ff9f450b4d5c746daa89e332c506114195c1ac5
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2022-06-06 15:38:31 +0200

    CLI WIP

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,23 @@
 import { program } from "commander";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { simulate } from "./simulate.js";
 
 const run = (netlist: string, options: Record<string, unknown>): void => {
-  console.log({ netlist, options });
+  const path = resolve(netlist);
+  let content: string;
+  try {
+    content = readFileSync(path, "utf-8");
+  } catch (err: any) {
+    if (err.code === "ENOENT") {
+      console.error(`File [${path}] not found.`);
+    } else {
+      console.error(`File [${path}] cannot be read: ${err.message}.`);
+    }
+    process.exitCode = 1;
+    return;
+  }
+  simulate(content);
 };
 
 program
