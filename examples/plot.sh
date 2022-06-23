0fab4d3361aee1416b38bf1e93222dd1d5b670b4
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2022-06-23 20:50:43 +0200

    Show processed files in the examples bash script

--- examples/plot.sh
+++ examples/plot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for f in *.netlist; do jssim --verbose "$f"; done
+for f in *.netlist; do echo "jssim $f"; jssim "$f"; done
 
-for f in *.plot; do gnuplot "$f"; done
+for f in *.plot; do echo "gnuplot $f"; gnuplot "$f"; done
