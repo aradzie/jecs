1fc1c7325cb72e3ce5466be80191bc3065c89aa9
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2025-02-19 09:20:20 +0100

    use the modern ecmascript syntax

--- packages/simulator/src/analysis/solver-ac.ts
+++ packages/simulator/src/analysis/solver-ac.ts
@@ -3,35 +3,34 @@ import { type Circuit, ComplexStamper } from "../circuit/index.js";
 import { logger } from "../util/logging.js";
 
 export class AcSolver {
-  private readonly circuit: Circuit;
-  private readonly sle: Sle;
-  private readonly stamper: ComplexStamper;
+  readonly #circuit: Circuit;
+  readonly #sle: Sle;
+  readonly #stamper: ComplexStamper;
 
   constructor(circuit: Circuit) {
-    this.circuit = circuit;
-    this.sle = new Sle(circuit.nodes.length * 2);
-    this.stamper = new ComplexStamper(this.sle.A, this.sle.b);
+    this.#circuit = circuit;
+    this.#sle = new Sle(circuit.nodes.length * 2);
+    this.#stamper = new ComplexStamper(this.#sle.A, this.#sle.b);
   }
 
   solve(): void {
     logger.simulationStarted();
-    this.circuit.initAc();
+    this.#circuit.initAc();
     logger.iterationStarted();
-    this.sle.clear();
-    this.circuit.loadAc(this.stamper);
-    this.sle.solve(SleMethod.Gauss);
-    this.saveSolution();
+    this.#sle.clear();
+    this.#circuit.loadAc(this.#stamper);
+    this.#sle.solve(SleMethod.Gauss);
+    this.#saveSolution();
     logger.iterationEnded();
-    this.circuit.endAc();
+    this.#circuit.endAc();
     logger.simulationEnded();
   }
 
-  private saveSolution(): void {
-    const { x } = this.sle;
-    for (const node of this.circuit.nodes) {
+  #saveSolution(): void {
+    for (const node of this.#circuit.nodes) {
       const { index } = node;
-      const xr = x[index * 2];
-      const xi = x[index * 2 + 1];
+      const xr = this.#sle.x[index * 2];
+      const xi = this.#sle.x[index * 2 + 1];
       switch (node.type) {
         case "node":
           node.voltage = cxMagnitude(xr, xi);
--- packages/simulator/src/analysis/solver-nonlinear.ts
+++ packages/simulator/src/analysis/solver-nonlinear.ts
@@ -37,93 +37,93 @@ export class NonlinearSolver {
     }),
   };
 
-  private readonly circuit: Circuit;
-  private readonly options: ConvergenceOptions;
-  private readonly sle: Sle;
-  private readonly backupX: Vector;
-  private readonly currX: Vector;
-  private readonly prevX: Vector;
-  private readonly currB: Vector;
-  private readonly prevB: Vector;
-  private readonly stamper: RealStamper;
-  private helper: ConvHelper;
-  private sourceFactor: number;
-  private gMin: number;
+  readonly #circuit: Circuit;
+  readonly #options: ConvergenceOptions;
+  readonly #sle: Sle;
+  readonly #backupX: Vector;
+  readonly #currX: Vector;
+  readonly #prevX: Vector;
+  readonly #currB: Vector;
+  readonly #prevB: Vector;
+  readonly #stamper: RealStamper;
+  #helper: ConvHelper;
+  #sourceFactor: number;
+  #gMin: number;
 
-  private init: () => void = () => {};
-  private load: (stamper: RealStamper) => void = () => {};
-  private end: () => void = () => {};
+  #init: () => void = () => {};
+  #load: (stamper: RealStamper) => void = () => {};
+  #end: () => void = () => {};
 
   constructor(circuit: Circuit, props: Props) {
-    this.circuit = circuit;
-    this.options = getConvergenceOptions(props);
-    this.sle = new Sle(circuit.nodes.length);
-    this.backupX = vecMake(this.sle.size);
-    this.currX = vecMake(this.sle.size);
-    this.prevX = vecMake(this.sle.size);
-    this.currB = vecMake(this.sle.size);
-    this.prevB = vecMake(this.sle.size);
-    this.stamper = new RealStamper(this.sle.A, this.sle.b);
-    this.helper = ConvHelper.None;
-    this.sourceFactor = 1;
-    this.gMin = 0;
+    this.#circuit = circuit;
+    this.#options = getConvergenceOptions(props);
+    this.#sle = new Sle(circuit.nodes.length);
+    this.#backupX = vecMake(this.#sle.size);
+    this.#currX = vecMake(this.#sle.size);
+    this.#prevX = vecMake(this.#sle.size);
+    this.#currB = vecMake(this.#sle.size);
+    this.#prevB = vecMake(this.#sle.size);
+    this.#stamper = new RealStamper(this.#sle.A, this.#sle.b);
+    this.#helper = ConvHelper.None;
+    this.#sourceFactor = 1;
+    this.#gMin = 0;
   }
 
-  useDc() {
-    this.init = () => {
-      this.circuit.initDc();
+  useDc(): void {
+    this.#init = () => {
+      this.#circuit.initDc();
     };
-    this.load = (stamper) => {
-      this.circuit.loadDc(stamper);
+    this.#load = (stamper) => {
+      this.#circuit.loadDc(stamper);
     };
-    this.end = () => {
-      this.circuit.endDc();
+    this.#end = () => {
+      this.#circuit.endDc();
     };
   }
 
-  useTr() {
-    this.init = () => {
-      this.circuit.initTr();
+  useTr(): void {
+    this.#init = () => {
+      this.#circuit.initTr();
     };
-    this.load = (stamper) => {
-      this.circuit.loadTr(stamper);
+    this.#load = (stamper) => {
+      this.#circuit.loadTr(stamper);
     };
-    this.end = () => {
-      this.circuit.endTr();
+    this.#end = () => {
+      this.#circuit.endTr();
     };
   }
 
   solve(): void {
     logger.simulationStarted();
-    this.solveNonLinear();
+    this.#solveNonLinear();
     logger.simulationEnded();
   }
 
-  private solveNonLinear(): void {
+  #solveNonLinear(): void {
     // Next strategy.
 
-    this.backupSolution();
-    vecClear(this.prevX);
-    vecClear(this.prevB);
-    if (this.solveNonLinear_Normal()) {
+    this.#backupSolution();
+    vecClear(this.#prevX);
+    vecClear(this.#prevB);
+    if (this.#solveNonLinear_Normal()) {
       return;
     }
 
     // Next strategy.
 
-    this.restoreSolution();
-    vecClear(this.prevX);
-    vecClear(this.prevB);
-    if (this.solveNonLinear_SourceStepping()) {
+    this.#restoreSolution();
+    vecClear(this.#prevX);
+    vecClear(this.#prevB);
+    if (this.#solveNonLinear_SourceStepping()) {
       return;
     }
 
     // Next strategy.
 
-    this.restoreSolution();
-    vecClear(this.prevX);
-    vecClear(this.prevB);
-    if (this.solveNonLinear_GMinStepping()) {
+    this.#restoreSolution();
+    vecClear(this.#prevX);
+    vecClear(this.#prevB);
+    if (this.#solveNonLinear_GMinStepping()) {
       return;
     }
 
@@ -132,49 +132,48 @@ export class NonlinearSolver {
     throw new ConvergenceError(`Simulation did not converge.`);
   }
 
-  private solveNonLinear_Normal(): boolean {
-    this.helper = ConvHelper.None;
-    this.sourceFactor = 1;
-    this.gMin = 0;
-    return this.iterate(this.options.maxIter);
+  #solveNonLinear_Normal(): boolean {
+    this.#helper = ConvHelper.None;
+    this.#sourceFactor = 1;
+    this.#gMin = 0;
+    return this.#iterate(this.#options.maxIter);
   }
 
-  private solveNonLinear_SourceStepping(): boolean {
+  #solveNonLinear_SourceStepping(): boolean {
     for (const sourceFactor of sourceFactorList) {
-      this.helper = ConvHelper.SourceStepping;
-      this.sourceFactor = sourceFactor;
-      this.gMin = 0;
-      if (!this.iterate(this.options.maxIter)) {
+      this.#helper = ConvHelper.SourceStepping;
+      this.#sourceFactor = sourceFactor;
+      this.#gMin = 0;
+      if (!this.#iterate(this.#options.maxIter)) {
         return false;
       }
     }
     return true;
   }
 
-  private solveNonLinear_GMinStepping(): boolean {
+  #solveNonLinear_GMinStepping(): boolean {
     for (const gMin of gMinList) {
-      this.helper = ConvHelper.GMinStepping;
-      this.sourceFactor = 1;
-      this.gMin = gMin;
-      if (!this.iterate(this.options.maxIter)) {
+      this.#helper = ConvHelper.GMinStepping;
+      this.#sourceFactor = 1;
+      this.#gMin = gMin;
+      if (!this.#iterate(this.#options.maxIter)) {
         return false;
       }
     }
     return true;
   }
 
-  private iterate(maxIter: number): boolean {
-    this.circuit.sourceFactor = this.sourceFactor;
-    const { currX, prevX, prevB, currB } = this;
-    this.startIteration();
+  #iterate(maxIter: number): boolean {
+    this.#circuit.sourceFactor = this.#sourceFactor;
+    this.#startIteration();
     let iter = 0;
     while (iter < maxIter) {
-      this.doIteration();
-      const conv = iter > 0 && this.converged();
-      vecCopy(currX, prevX);
-      vecCopy(currB, prevB);
+      this.#doIteration();
+      const conv = iter > 0 && this.#converged();
+      vecCopy(this.#currX, this.#prevX);
+      vecCopy(this.#currB, this.#prevB);
       if (conv) {
-        this.endIteration();
+        this.#endIteration();
         return true;
       }
       iter += 1;
@@ -182,63 +181,60 @@ export class NonlinearSolver {
     return false;
   }
 
-  private startIteration(): void {
-    this.init();
+  #startIteration(): void {
+    this.#init();
   }
 
-  private doIteration(): void {
+  #doIteration(): void {
     logger.iterationStarted();
-    this.sle.clear();
-    this.load(this.stamper);
-    if (this.helper === ConvHelper.GMinStepping) {
-      this.applyGMin();
+    this.#sle.clear();
+    this.#load(this.#stamper);
+    if (this.#helper === ConvHelper.GMinStepping) {
+      this.#applyGMin();
     }
-    vecCopy(this.sle.b, this.currB);
-    this.sle.solve(SleMethod.Gauss);
-    vecCopy(this.sle.x, this.currX);
-    this.saveSolution();
+    vecCopy(this.#sle.b, this.#currB);
+    this.#sle.solve(SleMethod.Gauss);
+    vecCopy(this.#sle.x, this.#currX);
+    this.#saveSolution();
     logger.iterationEnded();
   }
 
-  private endIteration(): void {
-    this.end();
+  #endIteration(): void {
+    this.#end();
   }
 
-  private applyGMin(): void {
-    const { sle, gMin } = this;
-    const { size, A } = sle;
-    for (let i = 0; i < size; i++) {
-      A[i][i] += gMin;
+  #applyGMin(): void {
+    for (let i = 0; i < this.#sle.size; i++) {
+      this.#sle.A[i][i] += this.#gMin;
     }
   }
 
-  private converged(): boolean {
-    const { currX, prevX, currB, prevB } = this;
-    const { abstol, vntol, reltol } = this.options;
-    for (const node of this.circuit.nodes) {
+  #converged(): boolean {
+    const { abstol, vntol, reltol } = this.#options;
+    for (const node of this.#circuit.nodes) {
       const { index } = node;
       switch (node.type) {
         case "node": {
-          const v1 = prevX[index];
-          const v2 = currX[index];
+          const v1 = this.#prevX[index];
+          const v2 = this.#currX[index];
           if (Math.abs(v2 - v1) >= reltol * Math.abs(v2) + vntol) {
             return false;
           }
-          const i1 = prevB[index];
-          const i2 = currB[index];
+          const i1 = this.#prevB[index];
+          const i2 = this.#currB[index];
           if (Math.abs(i2 - i1) >= reltol * Math.abs(i2) + abstol) {
             return false;
           }
           break;
         }
         case "branch": {
-          const i1 = prevX[index];
-          const i2 = currX[index];
+          const i1 = this.#prevX[index];
+          const i2 = this.#currX[index];
           if (Math.abs(i2 - i1) >= reltol * Math.abs(i2) + abstol) {
             return false;
           }
-          const v1 = prevB[index];
-          const v2 = currB[index];
+          const v1 = this.#prevB[index];
+          const v2 = this.#currB[index];
           if (Math.abs(v2 - v1) >= reltol * Math.abs(v2) + vntol) {
             return false;
           }
@@ -249,46 +245,43 @@ export class NonlinearSolver {
     return true;
   }
 
-  private saveSolution(): void {
-    const { currX } = this;
-    for (const node of this.circuit.nodes) {
+  #saveSolution(): void {
+    for (const node of this.#circuit.nodes) {
       const { index } = node;
       switch (node.type) {
         case "node":
-          node.voltage = currX[index];
+          node.voltage = this.#currX[index];
           break;
         case "branch":
-          node.current = currX[index];
+          node.current = this.#currX[index];
           break;
       }
     }
   }
 
-  private backupSolution(): void {
-    const { backupX } = this;
-    for (const node of this.circuit.nodes) {
+  #backupSolution(): void {
+    for (const node of this.#circuit.nodes) {
       const { index } = node;
       switch (node.type) {
         case "node":
-          backupX[index] = node.voltage;
+          this.#backupX[index] = node.voltage;
           break;
         case "branch":
-          backupX[index] = node.current;
+          this.#backupX[index] = node.current;
           break;
       }
     }
   }
 
-  private restoreSolution(): void {
-    const { backupX } = this;
-    for (const node of this.circuit.nodes) {
+  #restoreSolution(): void {
+    for (const node of this.#circuit.nodes) {
       const { index } = node;
       switch (node.type) {
         case "node":
-          node.voltage = backupX[index];
+          node.voltage = this.#backupX[index];
           break;
         case "branch":
-          node.current = backupX[index];
+          node.current = this.#backupX[index];
           break;
       }
     }
